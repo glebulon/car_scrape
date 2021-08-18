6e1f5cf46dd3bc2a62e16ed332726fa96960c99e
glebulon <gleb.zverinskiy@gmail.com>
2021-08-17 23:22:57 -0400

    DOn't screenshot on carfax exception

--- modules/car_fax.py
+++ modules/car_fax.py
@@ -23,7 +23,7 @@ def carfax_viewer(vin, driver):
         if not WebDriverWait(driver, 5).until(ec.presence_of_element_located((By.ID, "cfxHdrBar"))):
             logging.error(driver.page_source)
     except Exception as e:
-        driver.save_screenshot("screenshots/cfax/{}.png".format(vin))
+        # driver.save_screenshot("screenshots/cfax/{}.png".format(vin))
         carfax_login(driver)
         driver.get("https://www.carfaxonline.com/vhrs/{}".format(vin))
     soup = BeautifulSoup(driver.page_source, "lxml")
